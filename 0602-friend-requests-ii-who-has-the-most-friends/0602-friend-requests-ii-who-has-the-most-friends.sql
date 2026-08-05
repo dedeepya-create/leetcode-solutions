62503e40cd863d5d05b920828c4b7de40d5bc3ce
dedeepya <mushroomred933@gmail.com>
2026-08-05 21:25:38 +0530

    Time: 337 ms (43.47%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,7 @@
+select id , count(*) as num from
+            (select requester_id as id from requestaccepted
+            union all
+            select accepter_id as id from requestaccepted )as u
+group by id
+order by num desc
+limit 1
